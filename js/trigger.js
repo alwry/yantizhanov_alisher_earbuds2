ca33438685c62c2bc04213de30dffc740764424b
alwry <144924749+alwry@users.noreply.github.com>
2024-11-22 22:25:50 -0500

    Content
    Filled page with content

--- js/trigger.js
+++ js/trigger.js
@@ -2,19 +2,23 @@ const visionBoxes = document.querySelectorAll('.vision-box');
 const ancText = document.querySelector('.opacity');
 const ancDiv = document.querySelector('.anc');
 
-visionBoxes.forEach((visionBox) => {
-    gsap.to(visionBox, {
-        width: "50%", 
-        scrollTrigger: {
-            trigger: visionBox,
-            start: "top 50%",
-            end: "bottom 20%",
-            toggleActions: "restart none none reverse",
-            scrub: true,
-            markers: true, 
-        },
-    });
-});
+
+// visionBoxes.forEach((visionBox) => {
+//     gsap.to(visionBox, {
+//         width: "40%", 
+//         scrollTrigger: {
+//             trigger: visionBox,
+//             pin: true,
+//             start: "top 50%",
+//             end: "bottom 20%",
+//             toggleActions: "restart none none reverse",
+//             scrub: true,
+//             markers: true, 
+//         },
+//     });
+// });
+
+
 
 gsap.to(ancText, {
     color: "#000",
